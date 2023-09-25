36b0027c031d74963de7b600601be842f00bcda4
omar anas <omar_anas61@yahoo.com>
2023-09-25 23:20:27 +0300

    commenting body pix

--- public/js/Room.js
+++ public/js/Room.js
@@ -1283,10 +1283,10 @@ async function changeCamera(deviceId) {
         .getUserMedia(videoConstraints)
         .then((camStream) => {
             initVideo.className = 'mirror';
-            //initVideo.srcObject = camStream;
-            BodypixStream.srcObject = camStream;
+            initVideo.srcObject = camStream;
+            //BodypixStream.srcObject = camStream;
             initStream = camStream;
-            setResultStream()
+            //setResultStream()
             console.log(
                 '04.5 ----> Success attached init cam video stream',
                 initStream.getVideoTracks()[0].getSettings(),
@@ -1296,65 +1296,61 @@ async function changeCamera(deviceId) {
             console.error('[Error] changeCamera', err);
             userLog('error', 'Error while swapping camera' + err, 'top-end');
         });
-        BodypixStream.onloadedmetadata = () => {
+    //     BodypixStream.onloadedmetadata = () => {
             
-        webcamCanvas.width = BodypixStream.videoWidth;
-        webcamCanvas.height = BodypixStream.videoHeight;
-        tempCanvas.width = BodypixStream.videoWidth;
-        tempCanvas.height = BodypixStream.videoHeight;
+    //     webcamCanvas.width = BodypixStream.videoWidth;
+    //     webcamCanvas.height = BodypixStream.videoHeight;
+    //     tempCanvas.width = BodypixStream.videoWidth;
+    //     tempCanvas.height = BodypixStream.videoHeight;
         
-    };
+    // };
     
-    BodypixStream.addEventListener("loadeddata", segmentPersons);
+    // BodypixStream.addEventListener("loadeddata", segmentPersons);
 }
-function segmentPersons() {
-    tempCanvasCtx.drawImage(BodypixStream, 0, 0);
-    if (previousSegmentationComplete) {
-        previousSegmentationComplete = false;
-        // Now classify the canvas image we have available.
-        model.segmentPerson(tempCanvas, segmentationProperties)
-        .then(segmentation => {
-            processSegmentation(segmentation);
-            previousSegmentationComplete = true;
-            });
-        }
-        //Call this function repeatedly to perform segmentation on all frames of the video.
-        window.requestAnimationFrame(segmentPersons);
-    }
+// function segmentPersons() {
+//     tempCanvasCtx.drawImage(BodypixStream, 0, 0);
+//     if (previousSegmentationComplete) {
+//         previousSegmentationComplete = false;
+//         // Now classify the canvas image we have available.
+//         model.segmentPerson(tempCanvas, segmentationProperties)
+//         .then(segmentation => {
+//             processSegmentation(segmentation);
+//             previousSegmentationComplete = true;
+//             });
+//         }
+//         //Call this function repeatedly to perform segmentation on all frames of the video.
+//         window.requestAnimationFrame(segmentPersons);
+//     }
     
-function processSegmentation(segmentation) {
-    var imgData = tempCanvasCtx.getImageData(0, 0, webcamCanvas.width, webcamCanvas.height);
-    //Loop through the pixels in the image
-    for(let i = 0; i < imgData.data.length; i+=4) {
-        let pixelIndex = i/4;
-        //Make the pixel transparent if it does not belong to a person using the body-pix model's output data array.
-        //This removes all pixels corresponding to the background.
-        if(segmentation.data[pixelIndex] == 0) {
-            imgData.data[i + 3] = 0;
-        }
-    }
-      //Draw the updated image on the canvas
-      webcamCanvasCtx.putImageData(imgData, 0, 0);
+// function processSegmentation(segmentation) {
+//     var imgData = tempCanvasCtx.getImageData(0, 0, webcamCanvas.width, webcamCanvas.height);
+//     //Loop through the pixels in the image
+//     for(let i = 0; i < imgData.data.length; i+=4) {
+//         let pixelIndex = i/4;
+//         //Make the pixel transparent if it does not belong to a person using the body-pix model's output data array.
+//         //This removes all pixels corresponding to the background.
+//         if(segmentation.data[pixelIndex] == 0) {
+//             imgData.data[i + 3] = 0;
+//         }
+//     }
+//       //Draw the updated image on the canvas
+//       webcamCanvasCtx.putImageData(imgData, 0, 0);
 
       
-    }
+//     }
 
-function setResultStream() {
-    console.log('working setResultStream')
-<<<<<<< HEAD
-    const stream = webcamCanvas.captureStream(60);
-=======
-    const stream = webcamCanvas.captureStream();
->>>>>>> parent of 988cac1 (Revert "uncomment boodypix")
-    initVideo.srcObject = stream;
-    initStream =stream
-    initVideo.play();
+// function setResultStream() {
+//     console.log('working setResultStream')
+//     const stream = webcamCanvas.captureStream();
+//     initVideo.srcObject = stream;
+//     initStream =stream
+//     initVideo.play();
 
     
-    if (initVideo.hidden) {
-        initVideo.hidden = false;
-    }
-}
+//     if (initVideo.hidden) {
+//         initVideo.hidden = false;
+//     }
+// }
 
 
 async function toggleScreenSharing() {
