82bfd6d9f003ec7929ccbbaa5be9a516b43d6f98
omar anas <omar_anas61@yahoo.com>
2023-09-27 05:49:53 +0300

    changin everything to normal

--- public/js/RoomClient.js
+++ public/js/RoomClient.js
@@ -820,7 +820,8 @@ class RoomClient {
                 }else if(initStream===null){
                     stream  = await navigator.mediaDevices.getUserMedia(mediaConstraints);
                 }else{
-                    stream = initStream;
+                    stream  = await navigator.mediaDevices.getUserMedia(mediaConstraints);
+                    //stream = initStream;
                 }
                 
             }
